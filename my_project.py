eb5cfcf0efc7d29b8e41154cbbc7b56a0d0573a0
AlexeyKadyshev <127925236+AlexeyKadyshev@users.noreply.github.com>
2023-11-26 09:51:22 +0300

    Update my_project.py
    Add recursion

--- my_project.py
+++ my_project.py
@@ -5,3 +5,11 @@ def hello_word():
 hello_word()
 
 # Необходимо реализовать рекурсивное выполнение функции hello_word, пока num > 0
+
+def rec_h_w(num):
+    if num == 0:
+        return None
+    else:
+        hello_word()
+        rec_h_w(num - 1)
+
